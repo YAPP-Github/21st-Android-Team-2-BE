e94825f16578465ee7ef9908595d7c95631d48c7
osandra <okhw97@gmail.com>
2022-11-16 21:39:48 +0900

    Initial commit
    Co-authored-by: jihyehann <haneh0714@gmail.com>

---
+++ src/main/kotlin/com/yapp/itemfinder/ItemFinderApplication.kt
@@ -0,0 +1,11 @@
+package com.yapp.itemfinder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItemFinderApplication
+
+fun main(args: Array<String>) {
+	runApplication<ItemFinderApplication>(*args)
+}
