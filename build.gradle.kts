76947211a109ddc8b4756c2a332d8c1d52f5821a
Jihye Han <haneh0714@gmail.com>
2022-11-18 01:27:43 +0900

    [A2Y-17] Spring Boot 프로젝트와 테스트 세팅 완료했습니다. (#1)
    * feat: Spring boot 프로젝트, 테스트 세팅
    
    * chore: allOpen, noArg 설정 추가
    
    * chore: 테스트 환경 인메모리 h2 데이터베이스 설정 수정
    
    - MODE=MySQL: h2 sql 문법을 MySQL 문법으로 변경
    - DB_CLOSE_DELAY=-1: jvm이 살아있는 동안 메모리 내 데이터베이스의 내용을 유지
    - DB_CLOSE_ON_EXIT=FALSE: db 자동 종료 비활성화
    
    * style: 파일 마지막 공백 라인 추가 - No newline at end of file 이슈 해결
    
    * Update gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,20 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val querydslVersion: String by project
+
 plugins {
 	id("org.springframework.boot") version "2.7.5"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
+	kotlin("kapt") version "1.7.10" //Querydsl
 }
 
 group = "com.yapp"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
+
 
 repositories {
 	mavenCentral()
@@ -23,6 +27,26 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+	//querydsl
+	implementation("com.querydsl:querydsl-jpa:${querydslVersion}")
+	kapt("com.querydsl:querydsl-apt:${querydslVersion}:jpa")
+
+	//h2
+	testImplementation("com.h2database:h2")
+
+}
+
+noArg {
+	annotation("javax.persistence.Entity")
+}
+
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
 }
 
 tasks.withType<KotlinCompile> {
