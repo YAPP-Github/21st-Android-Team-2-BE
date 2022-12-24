3ccf8d8cfa2c0756ea5a992960afc30c4bbf45c8
Han Jihye <haneh0714@gmail.com>
2022-12-24 23:06:43 +0900

    feat: add custom exception, error response, global exception handler

---
+++ src/main/kotlin/com/yapp/itemfinder/api/exception/ErrorResponse.kt
@@ -0,0 +1,11 @@
+package com.yapp.itemfinder.api.exception
+
+import com.fasterxml.jackson.annotation.JsonInclude
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class ErrorResponse (
+    val message: String? = null,
+    val code: String? = null
+)
+
+enum class ErrorCode(val value: String)
