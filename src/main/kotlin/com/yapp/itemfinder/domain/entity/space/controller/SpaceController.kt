0acc50bad8373b57150f7da838bdc60567e42d6f
osandra <okhw97@gmail.com>
2023-01-01 22:30:29 +0900

    feat: 공간 추가/조회 api 관련 swagger 세팅 및 springdoc 관련 코틀린 라이브러리 추가

--- src/main/kotlin/com/yapp/itemfinder/domain/entity/space/controller/SpaceController.kt
+++ src/main/kotlin/com/yapp/itemfinder/domain/entity/space/controller/SpaceController.kt
@@ -5,6 +5,7 @@ import com.yapp.itemfinder.domain.entity.member.MemberEntity
 import com.yapp.itemfinder.domain.entity.space.dto.CreateSpaceRequest
 import com.yapp.itemfinder.domain.entity.space.dto.SpacesResponse
 import com.yapp.itemfinder.domain.entity.space.service.SpaceService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 class SpaceController(
     private val spaceService: SpaceService,
 ) {
+    @Operation(summary = "새로운 공간 등록")
     @PostMapping("/spaces")
     fun createSpace(@LoginMember member: MemberEntity, @RequestBody createSpaceReq: CreateSpaceRequest) {
         spaceService.createSpace(
@@ -21,7 +23,7 @@ class SpaceController(
             member = member
         )
     }
-
+    @Operation(summary = "멤버가 등록한 공간 리스트 조회")
     @GetMapping("/spaces")
     fun getSpaces(@LoginMember member: MemberEntity): SpacesResponse {
         return spaceService.getSpaces(member.id)
