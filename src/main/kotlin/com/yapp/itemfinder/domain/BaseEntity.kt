32f2db104df625fea4f08fedbc51c6cbec70326d
Han Jihye <haneh0714@gmail.com>
2023-01-07 16:01:53 +0900

    feat: BaseEntity equals(), hashCode() 추가

--- src/main/kotlin/com/yapp/itemfinder/domain/BaseEntity.kt
+++ src/main/kotlin/com/yapp/itemfinder/domain/BaseEntity.kt
@@ -26,4 +26,19 @@ abstract class BaseEntity(
     @LastModifiedDate
     @Column(updatable = true)
     var updatedAt: LocalDateTime = LocalDateTime.now(KST_ZONE_ID)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BaseEntity
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
 }
