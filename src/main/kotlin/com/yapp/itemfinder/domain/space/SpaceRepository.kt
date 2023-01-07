88e96d7051e4a7ffb645c1aeb31b7a0db86b0929
osandra <okhw97@gmail.com>
2023-01-07 18:19:12 +0900

    feat: 특정 멤버가 등록한 공간에 대한 정보 및 보관함 수 조회하는 부분 추가

--- src/main/kotlin/com/yapp/itemfinder/domain/space/SpaceRepository.kt
+++ src/main/kotlin/com/yapp/itemfinder/domain/space/SpaceRepository.kt
@@ -1,8 +1,43 @@
 package com.yapp.itemfinder.domain.space
 
+import com.querydsl.core.types.Projections
+import com.querydsl.jpa.impl.JPAQueryFactory
+import com.yapp.itemfinder.domain.container.QContainerEntity.containerEntity
+import com.yapp.itemfinder.domain.space.QSpaceEntity.spaceEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface SpaceRepository : JpaRepository<SpaceEntity, Long> {
+interface SpaceRepository : JpaRepository<SpaceEntity, Long>, SpaceRepositorySupport {
     fun findByMemberIdAndName(memberId: Long, name: String): SpaceEntity?
     fun findByMemberId(memberId: Long): List<SpaceEntity>
 }
+
+interface SpaceRepositorySupport {
+    fun getSpaceWithContainerCountByMemberId(memberId: Long): List<SpaceWithContainerCount>
+}
+
+class SpaceRepositorySupportImpl(
+    private val queryFactory: JPAQueryFactory
+): SpaceRepositorySupport {
+    override fun getSpaceWithContainerCountByMemberId(memberId: Long): List<SpaceWithContainerCount> {
+        return queryFactory.select(
+                Projections.constructor(
+                    SpaceWithContainerCount::class.java,
+                    spaceEntity.id,
+                    spaceEntity.name,
+                    containerEntity.id.count()
+                )
+            )
+            .from(spaceEntity)
+            .leftJoin(containerEntity).on(containerEntity.space.id.eq(spaceEntity.id))
+            .where(spaceEntity.member.id.eq(memberId))
+            .groupBy(spaceEntity.id)
+            .orderBy(spaceEntity.createdAt.asc())
+            .fetch()
+    }
+}
+
+data class SpaceWithContainerCount(
+    val spaceId: Long,
+    val spaceName: String,
+    val containerCount: Long
+)
