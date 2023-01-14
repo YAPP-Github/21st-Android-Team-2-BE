5746208b32210de18d1c9f63324a231aaa89b987
osandra <okhw97@gmail.com>
2023-01-14 09:47:37 +0900

    feat: 보관함 등록 시 이름 공백 검증 추가

--- src/main/kotlin/com/yapp/itemfinder/domain/container/dto/CreateContainerRequest.kt
+++ src/main/kotlin/com/yapp/itemfinder/domain/container/dto/CreateContainerRequest.kt
@@ -5,10 +5,13 @@ import com.yapp.itemfinder.api.exception.BadRequestException
 import com.yapp.itemfinder.domain.container.ContainerEntity.Companion.CONTAINER_NAME_LENGTH_LIMIT
 import com.yapp.itemfinder.domain.container.IconType
 import org.hibernate.validator.constraints.URL
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
 class CreateContainerRequest(
     val spaceId: Long,
+
+    @field:NotBlank
     @field:Size(min = 1, max = CONTAINER_NAME_LENGTH_LIMIT, message = "1자 이상 ${CONTAINER_NAME_LENGTH_LIMIT}자 이내로 이름을 등록해 주세요.")
     val name: String,
     @JsonProperty(value = "icon")
