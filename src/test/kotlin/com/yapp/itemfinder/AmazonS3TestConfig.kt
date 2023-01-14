56ab067319910278bc039bb890c325ea86680dd5
Han Jihye <haneh0714@gmail.com>
2023-01-15 03:37:45 +0900

    test: aws region 수정

--- src/test/kotlin/com/yapp/itemfinder/AmazonS3TestConfig.kt
+++ src/test/kotlin/com/yapp/itemfinder/AmazonS3TestConfig.kt
@@ -3,7 +3,6 @@ package com.yapp.itemfinder
 import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.AnonymousAWSCredentials
 import com.amazonaws.client.builder.AwsClientBuilder
-import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import io.findify.s3mock.S3Mock
@@ -17,7 +16,7 @@ import javax.annotation.PreDestroy
 @TestConfiguration
 class AmazonS3TestConfig {
     private val port = 8001
-    private val region = Regions.AP_NORTHEAST_2.name
+    private val region = "ap-northeast-2"
     @Value("\${cloud.aws.s3.bucket}")
     private val bucket = ""
     private lateinit var s3Mock: S3Mock
