9d63eb43f07df602c2adaa6acebcecc9d1a20a92
Han Jihye <haneh0714@gmail.com>
2023-01-26 20:10:10 +0900

    refactor: TagsResponse에 TagEntity -> TagsResponse 변환 메서드(from) 추가

--- src/main/kotlin/com/yapp/itemfinder/domain/tag/TagService.kt
+++ src/main/kotlin/com/yapp/itemfinder/domain/tag/TagService.kt
@@ -2,7 +2,6 @@ package com.yapp.itemfinder.domain.tag
 
 import com.yapp.itemfinder.domain.member.MemberEntity
 import com.yapp.itemfinder.domain.tag.dto.CreateTagsRequest
-import com.yapp.itemfinder.domain.tag.dto.TagResponse
 import com.yapp.itemfinder.domain.tag.dto.TagsResponse
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -17,7 +16,7 @@ class TagService(
         val tagNames = request.tags.map { it.name }.distinct()
         val existingTags = findExistingTags(member, tagNames)
         val newTags = createTags(member, tagNames.filterNot { existingTags.map { t -> t.name }.contains(it) })
-        return TagsResponse((existingTags + newTags).map { TagResponse(it) })
+        return TagsResponse.from(existingTags + newTags)
     }
 
     private fun findExistingTags(member: MemberEntity, tagNames: List<String>): List<TagEntity> {
--- src/main/kotlin/com/yapp/itemfinder/domain/tag/dto/TagsResponse.kt
+++ src/main/kotlin/com/yapp/itemfinder/domain/tag/dto/TagsResponse.kt
@@ -1,5 +1,11 @@
 package com.yapp.itemfinder.domain.tag.dto
 
+import com.yapp.itemfinder.domain.tag.TagEntity
+
 data class TagsResponse(
     val tags: List<TagResponse>
-)
+) {
+    companion object {
+        fun from(tags: List<TagEntity>): TagsResponse = TagsResponse(tags.map { TagResponse(it) })
+    }
+}
